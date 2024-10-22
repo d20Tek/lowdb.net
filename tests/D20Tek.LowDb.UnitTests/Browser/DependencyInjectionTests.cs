ab0d19ceb09ae79b9d0df1b401b642e9e9eb1528
Pedro Silva <DarthPedro@gmail.com>
2024-10-22 15:38:53 -0700

    Added unit tests for LocalLowDb and Async dependency registration functions. Added FakeJSRuntime helper class to abstract IJSRuntime requirement out of tests.

---
+++ tests/D20Tek.LowDb.UnitTests/Browser/DependencyInjectionTests.cs
@@ -0,0 +1,63 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using Blazored.LocalStorage;
+using D20Tek.LowDb.Browser;
+using D20Tek.LowDb.UnitTests.Entities;
+using D20Tek.LowDb.UnitTests.Fakes;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+
+namespace D20Tek.LowDb.UnitTests.Browser;
+
+[TestClass]
+public class DependencyInjectionTests
+{
+    [TestMethod]
+    public void AddLocalLowDb_AddsDbAndLocalStorageAdapter()
+    {
+        // arrange
+        var keyname = "test-key-1";
+        var services = CreateServiceCollection();
+
+        // act
+        services.AddLocalLowDb<TestDocument>(keyname);
+
+        // assert
+        services.Any(x => x.ServiceType == typeof(LowDb<TestDocument>)).Should().BeTrue();
+
+        // act on service provider
+        var provider = services.BuildServiceProvider();
+
+        // assert
+        var lowdb = provider.GetService<LowDb<TestDocument>>();
+        lowdb.Should().NotBeNull();
+    }
+
+    [TestMethod]
+    public void AddLowDbAsync_AddsDbAndJsonFileAdapter()
+    {
+        // arrange
+        var keyname = "test-key-2";
+        var services = CreateServiceCollection();
+
+        // act
+        services.AddLocalLowDbAsync<TestDocument>(keyname, ServiceLifetime.Singleton);
+
+        // assert
+        services.Any(x => x.ServiceType == typeof(LowDbAsync<TestDocument>)).Should().BeTrue();
+
+        // act on service provider
+        var provider = services.BuildServiceProvider();
+
+        // assert
+        var lowdb = provider.GetService<LowDbAsync<TestDocument>>();
+        lowdb.Should().NotBeNull();
+    }
+
+    private static IServiceCollection CreateServiceCollection() =>
+        new ServiceCollection()
+            .AddSingleton<IJSRuntime, FakeJSRuntime>()
+            .AddBlazoredLocalStorage();
+}
---
+++ tests/D20Tek.LowDb.UnitTests/Fakes/FakeJSRuntime.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+using System.Diagnostics.CodeAnalysis;
+
+namespace D20Tek.LowDb.UnitTests.Fakes;
+
+[ExcludeFromCodeCoverage]
+internal class FakeJSRuntime : IJSRuntime
+{
+    public ValueTask<TValue> InvokeAsync<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.PublicFields | DynamicallyAccessedMemberTypes.PublicProperties)] TValue>
+        (string identifier, object?[]? args)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<TValue> InvokeAsync<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.PublicFields | DynamicallyAccessedMemberTypes.PublicProperties)] TValue>
+        (string identifier, CancellationToken cancellationToken, object?[]? args)
+    {
+        throw new NotImplementedException();
+    }
+}
