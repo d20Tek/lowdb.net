a134ba0ff03679c0a572e70de2dabb377b750425
Pedro Silva <DarthPedro@gmail.com>
2025-05-14 15:54:36 -0700

    Created the d20Tek.LowDb.Repositories project. Added definition of IEntity and IRepository interfaces. Entered package information for new project.

---
+++ src/D20Tek.LowDb.Repositories/IEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace D20Tek.LowDb.Repositories;
+
+public interface IEntity<TId> where TId : notnull, IEquatable<TId>
+{
+    public TId Id { get; }
+
+    public void SetId(TId id);
+}
---
+++ src/D20Tek.LowDb.Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using D20Tek.Functional;
+
+namespace D20Tek.LowDb.Repositories;
+
+public interface IRepository<TEntity, TId>
+    where TEntity : IEntity<TId>
+    where TId : notnull, IEquatable<TId>
+{
+    Result<TEntity> Create(TEntity entity);
+
+    Result<TEntity> Delete(TId id);
+
+    Result<TEntity[]> DeleteMany(TEntity[] entities);
+
+    TEntity[] GetEntities();
+
+    Result<TEntity> GetEntityById(TId id);
+
+    Result<TEntity> Update(TEntity entity);
+}
